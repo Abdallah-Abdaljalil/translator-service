600267fd1a834f70d65f1c63b2447f9752563e15
Abdallah-Abdaljalil <aabdalja@andrew.cmu.edu>
2024-11-18 09:28:29 +0300

    Added mock tests

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,5 +1,7 @@
 from src.translator import translate_content
 from sentence_transformers import SentenceTransformer, util
+from mock import patch
+
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def eval_single_response_complete(expected_answer: tuple[bool, str], llm_response: tuple[bool, str]) -> float:
@@ -20,3 +22,42 @@ def test_llm_normal_response():
 
 def test_llm_gibberish_response():
     eval_single_response_complete((False, "Unintelligible"), translate_content("sdgkl fdkg ldfkgj dflkgj"))
+
+@patch.object(openai.ChatCompletion, 'create')
+def test_unexpected_language(mocker):
+  # we mock the model's response to return a random message
+  mocker.return_value.choices[0].message.content = "I don't understand your request"
+
+  # TODO assert the expected behavior
+  assert translate_content("Hier ist dein erstes Beispiel.") == (False, "Unavailable")
+
+@patch.object(openai.ChatCompletion, 'create')
+def test_unexpected_return(mocker):
+  # we mock the model's response to return a random message
+  mocker.return_value.choices[0].message.content = "dhkajshfljaelh"
+
+  # TODO assert the expected behavior
+  assert translate_content("Hier ist dein erstes Beispiel.") == (False, "Unavailable")
+
+@patch.object(openai.ChatCompletion, 'create')
+def test_unexpected_char(mocker):
+  # we mock the model's response to return a random message
+  mocker.return_value.choices[0].message.content = "#@&^$#&^@%$&"
+
+  # TODO assert the expected behavior
+  assert translate_content("Hier ist dein erstes Beispiel.") == (False, "Unavailable")
+
+@patch.object(openai.ChatCompletion, 'create')
+def test_unexpected_random(mocker):
+  # we mock the model's response to return a random message
+  mocker.return_value.choices[0].message.content = "ءايخاخثبخثم"
+
+  # TODO assert the expected behavior
+  assert translate_content("Hier ist dein erstes Beispiel.") == (False, "Unavailable")
+
+@patch.object(openai.ChatCompletion, 'create')
+def test_unexpected_empty(mocker):
+  # we mock the model's response to return a random message
+  mocker.return_value.choices[0].message.content = "."
+  # TODO assert the expected behavior
+  assert translate_content("Hier ist dein erstes Beispiel.") == (False, "Unavailable")
