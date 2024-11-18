621417ea73e86f4a7a1db1739c6769d0db074844
Abdallah-Abdaljalil <aabdalja@andrew.cmu.edu>
2024-11-18 15:17:21 +0300

    Trying with a test

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -16,6 +16,11 @@ from src.translator import translate_content
 # def test_chinese():
 #     eval_single_response_complete((False, "This is a message in Chinese"), translate_content("这是一条中文消息"))
 
+def test_chinese():
+    is_english, translated_content = translate_content("这是一条中文消息")
+    assert is_english == False
+    assert translated_content == "This is a Chinese message"
+
 # def test_llm_normal_response():
 #     eval_single_response_complete((False, "Here is your first example."), translate_content("Hier ist dein erstes Beispiel."))
 
